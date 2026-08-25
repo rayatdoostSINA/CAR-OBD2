8f74b696ddcae63791d9f339ac52e852df8e23f4
rayatdoostSINA <sina.rayatdoost@gmail.com>
2026-08-25 16:31:14 +0330

    refactor: update root component to App
    Switch the entry point from the Home page component to the main App component to ensure consistent application initialization.

--- github-pages-src/main.tsx
+++ github-pages-src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Home from '../app/page';
+import App from '../src/App';
 import '../app/globals.css';
 import '../app/extras.css';
 
-createRoot(document.getElementById('root')!).render(<StrictMode><Home /></StrictMode>);
+createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
